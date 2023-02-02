453f43a170ed920402d1b280691ac25d1312bd72
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-02-02 08:24:55 -0300

    feature: adding global props values

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,6 @@
 import {  createGlobalStyle } from 'styled-components';
 
-export default createGlobalStyle `
+export default createGlobalStyle`
 * {
     margin: 0;
     padding: 0;
@@ -8,9 +8,9 @@ export default createGlobalStyle `
 }
 
 body {
-    background: #f5f5f5;
+    background: ${(props) => props.theme.colors.background};
     font-size: 14px;
-    color: #333;
+    color: ${(props) => props.theme.colors.text};
     font-family: sans-serif;
 }
 `;
