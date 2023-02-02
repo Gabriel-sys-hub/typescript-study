90674d9ba24d2c882c95aea57a0d7482fd0dd79f
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-02-02 10:32:42 -0300

    feature: removing switch offColor.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ const Header: React.FC<Props> = ({toggleTheme}) => {
         <Container>
             Hello World
 
-            <Switch offColor={colors.primary} onColor={colors.secondary} height={ 10 } width={30} handleDiameter={20} onChange={toggleTheme} checked={title === "dark"} checkedIcon={false} uncheckedIcon={false}/>
+            <Switch  onColor={colors.secondary} height={ 10 } width={30} handleDiameter={20} onChange={toggleTheme} checked={title === "dark"} checkedIcon={false} uncheckedIcon={false}/>
         </Container>
     );
 }
