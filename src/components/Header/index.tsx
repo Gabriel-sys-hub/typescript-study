3af699d3436659d312fc770118edad5214c43796
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-02-02 08:25:31 -0300

    feature: adding switch on header, with values.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {Container} from './styles';
+import Switch from 'react-switch';
+import { ThemeContext } from 'styled-components';
 
-const Header: React.FC = () => {
+interface Props {
+    toggleTheme(): void;
+}
+
+const Header: React.FC<Props> = ({toggleTheme}) => {
+
+    const { colors, title } = useContext(ThemeContext);
     return (
         <Container>
-            
+            Hello World
+
+            <Switch offColor={colors.primary} onColor={colors.secondary} height={ 10 } width={30} handleDiameter={20} onChange={toggleTheme} checked={title === "dark"} checkedIcon={false} uncheckedIcon={false}/>
         </Container>
     );
 }
