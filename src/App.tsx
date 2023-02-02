598ba408445c953f79bf8688c03cad637ae92f15
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-02-02 08:25:50 -0300

    feature: adding toggletheme, and temeprovider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react"
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+import { ThemeProvider, DefaultTheme } from "styled-components";
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
+import usePersistedState from "./utils/usePersistedState";
 function App() {
 
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  }
+
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <GlobalStyle />
-     <Header />
+     <Header toggleTheme={toggleTheme}/>
     </div>
+    </ThemeProvider>
   )
 }
 
